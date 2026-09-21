e4952affdff368ed0edeaf5403d3d5a14d2b77e1
Zinat <ziinatullo@gmail.com>
2026-09-21 17:41:00 +0600

    host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6-0f7r_j)dp(4t*fvqtj-3w*ayvoa%tt388g=61l8fir2nq^%)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
