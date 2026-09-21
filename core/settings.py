6578c641f6f9e32971bdbb68273ac8c1442b45a8
Zinat <ziinatullo@gmail.com>
2026-09-21 17:32:16 +0600

    anywhere

--- core/settings.py
+++ core/settings.py
@@ -119,9 +119,15 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [     BASE_DIR / "static", ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -143,4 +149,3 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
- 
