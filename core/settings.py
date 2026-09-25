3b84f804edd5f795064dc1ef6d83ec4f80597c80
Ergeshov-Ernazar <ernisergesov99@gmail.com>
2026-09-25 17:33:22 +0600

    qw

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6-0f7r_j)dp(4t*fvqtj-3w*ayvoa%tt388g=61l8fir2nq^%)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
+
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [     BASE_DIR / "static", ]
 
